38871437a3335a533b090b87132f303d7d424606
OlegKushnir <kushnir.oleg87@gmail.com>
2022-09-23 15:11:06 +0300

    Errors

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,15 +4,14 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 
-const galleryContainer = document.querySelector('ul.gallery');
+const galleryContainer = document.querySelector('div.gallery');
 galleryContainer.insertAdjacentHTML("beforeend", createGalleryCard(galleryItems));
 
 function createGalleryCard(obj = {}) {
     let galleryCards = '';
     obj.forEach(({preview, original, description}) => 
     {
-    const galleryCard= `
-<a class="gallery__item" href="${original}">
+    const galleryCard= `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
 `;
@@ -21,13 +20,6 @@ galleryCards += galleryCard;
 return galleryCards;
 }
 
-galleryContainer.addEventListener('click', showImage);
 
 let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt',
     captionPosition:'bottom', captionDelay: 250});
-function showImage (event) {
-    event.preventDefault();
-    gallery.on('show.simplelightbox', function () {
-        
-    });
-}
